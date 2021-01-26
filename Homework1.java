92c2af8d8a3e8b6842c3832a5a03e09502ea7ac7
ZukaEnni <75634139+ZukaEnni@users.noreply.github.com>
2021-01-26 17:14:40 +0200

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,48 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        // My Calculator
+        int calc = 2021;
+        calc -=2000;
+        calc +=9;
+        calc *=3;
+        calc /=9;
+        System.out.println("My calculator says " + calc);
+
+        int a = 5;
+        int b = 6;
+        int calc1;
+        calc1 = a * b;
+        System.out.println(calc1);
+        calc1 = a - b;
+        System.out.println(calc1);
+        calc1 = a / b;
+        System.out.println(calc1);
+        calc1 = a + b;
+        System.out.println(calc1);
+
+        // Figures
+        // Square
+        int side = 5;
+        int math = side * 4;
+        System.out.println(math);
+
+        //Triangle
+        int side1 = 7;
+        int side2 = 6;
+        int side3 = 5;
+        int sum = side1 + side2 + side3;
+        System.out.println(sum);
+
+        //Circle
+        int pi = (int) 3.14;
+        int circleLine = 8;
+        int equation = pi + circleLine * 2;
+        System.out.println(equation);
+
+
+
+
+
+    }
+
+}
